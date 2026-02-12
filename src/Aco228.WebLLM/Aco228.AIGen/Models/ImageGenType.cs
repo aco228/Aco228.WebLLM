8a3cf7449283feb3daea964505a7712d227a8287
Aleksandar Konatar <aleksandar.k03@gmail.com>
2026-02-12 21:08:51 +0100

    schedule imgs for imageset

---
+++ src/Aco228.WebLLM/Aco228.AIGen/Models/ImageGenType.cs
@@ -0,0 +1,22 @@
+﻿namespace Aco228.AIGen.Models;
+
+public enum ImageGenType
+{
+    // openai
+    gpt4o = 1,
+    gpt1 = 2,
+    gpt_image_1mini = 3,
+    
+    grok_imagine_1 = 4,
+    
+    
+    poyo_gpt4o = 5,
+    poyo_gpt1_5 = 6,
+    poyo_nano_banana = 7,
+    poyo_mano_banana2 = 8,
+    poyo_flux2Pro = 9,
+    poyo_flux2Flex = 10,
+    poyo_seadream4_5 = 11,
+    poyo_z_image = 12,
+    poyo_grok = 13,
+}
