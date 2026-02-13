ccbf15402158fc33a457bb5976185e530166d348
Aleksandar Konatar <aleksandar.k03@gmail.com>
2026-02-13 17:40:19 +0100

    start with img gen task

--- src/Aco228.WebLLM/Aco228.AIGen/Services/IImgGenManager.cs
+++ src/Aco228.WebLLM/Aco228.AIGen/Services/IImgGenManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Concurrent;
 using Aco228.AIGen.Models;
 using Aco228.Common;
+using Aco228.Common.Extensions;
+using Aco228.Common.Infrastructure;
 using Aco228.Common.Models;
 
 namespace Aco228.AIGen.Services;
@@ -10,6 +12,9 @@ public interface IImgGenManager : ISingleton
     List<ImageGenProvider> Providers { get; }
     List<ModelImageDefinition> ModelDefinitions { get; }
     List<string> Models { get; }
+    ModelImageDefinition? GetModelDefinition(Enum apiName);
+    ManagedList<ModelImageDefinition> FilterSuitableModels(List<ImageGenProvider> providers, List<string> modelNames);
+    Task<List<GenerateImageResponse>> Generate(GenerateImageRequest prompt);
 }
 
 public class ImgGenManager : IImgGenManager
@@ -20,11 +25,42 @@ public class ImgGenManager : IImgGenManager
     public List<ModelImageDefinition> ModelDefinitions => _models.ToList();
     public List<string> Models => _models.Select(x => x.ModelApiName).ToList();
     
+    public ModelImageDefinition? GetModelDefinition(Enum apiName)
+    {
+        var modelname = ModelTypeHelper.GetModelApiName(apiName);
+        return _models.FirstOrDefault(x => x.ModelApiName == modelname);
+    }
+    
     public void RegisterGenerator<T>(ImageGenProvider provider, List<ModelImageDefinition> models)
         where T : IImgGen
     {
         var generator = ServiceProviderHelper.GetServiceByType(typeof(T)) as IImgGen;
         _generators.TryAdd(provider, generator);
         _models.AddRange(models);
+    }
+    
+    public ManagedList<ModelImageDefinition> FilterSuitableModels(List<ImageGenProvider> providers, List<string> modelNames)
+    {
+        return _models
+            .Where(x => providers.Contains(x.Provider))
+            .Where(x => modelNames.Contains(x.ModelApiName))
+            .ToManagedList();
+    }
+
+    public async Task<List<GenerateImageResponse>> Generate(GenerateImageRequest prompt)
+    {
+        ModelImageDefinition? modelDefinition = null;
+        if(!string.IsNullOrEmpty(prompt.ModelName))
+            modelDefinition = _models.FirstOrDefault(x => x.ModelApiName == prompt.ModelName);
+        else if(prompt.Provider != null)
+            modelDefinition = _models.Shuffle().FirstOrDefault(x => x.Provider == prompt.Provider);
+        else
+            modelDefinition = _models.Shuffle().First();
+        
+        if(modelDefinition == null)
+            throw new Exception("No suitable model found");
+        
+        var generator = _generators[modelDefinition.Provider];
+        return await generator.Generate(prompt);       
     }
 }
