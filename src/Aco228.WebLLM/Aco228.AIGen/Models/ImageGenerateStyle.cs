444bf6a3ed9c150d23e03c016ca12d59cd756ea6
Aleksandar Konatar <aleksandar.k03@gmail.com>
2026-02-20 23:05:25 +0100

    create image style spec ai

--- src/Aco228.WebLLM/Aco228.AIGen/Models/ImageGenerateStyle.cs
+++ src/Aco228.WebLLM/Aco228.AIGen/Models/ImageGenerateStyle.cs
@@ -1,8 +1,15 @@
-﻿namespace Aco228.AIGen.Models;
+﻿using Aco228.AIGen.Attributes;
+
+namespace Aco228.AIGen.Models;
 
 public class ImageGenerateStyle
 {
+    [PromptHint("What would be the best prompt to generate background image",
+        "Generate short 1 sentence (maximum 15 words) with generic meta prompt to generate best image for given topic")]
     public string About { get; set; }
+    
+    [PromptHint("What style suits best this kind of image?")]
+    [PromptExample("photography")]
     public string Style { get; set; } = "realistic";
 
     public bool IsValid()
