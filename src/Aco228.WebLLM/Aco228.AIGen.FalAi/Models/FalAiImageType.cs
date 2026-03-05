b24373ca517b394ab0b9bac6965eddc42fddcfad
Aleksandar Konatar <aleksandar.k03@gmail.com>
2026-03-05 12:20:34 +0100

    seadream pipeline

--- src/Aco228.WebLLM/Aco228.AIGen.FalAi/Models/FalAiImageType.cs
+++ src/Aco228.WebLLM/Aco228.AIGen.FalAi/Models/FalAiImageType.cs
@@ -4,9 +4,9 @@ namespace Aco228.AIGen.FalAi.Models;
 
 public enum FalAiImageType
 {
-    [ModelType("fal-ai/bytedance/seedream/v5/lite/text-to-image")]
+    [ModelType("bytedance/seedream/v5/lite/text-to-image")]
     Seadream5_lite,
     
-    [ModelType("fal-ai/bytedance/seedream/v4.5/text-to-image")]
+    [ModelType("bytedance/seedream/v4.5/text-to-image")]
     Seadream4_5,
 }
--- src/Aco228.WebLLM/Aco228.AIGen.FalAi/ServiceExtensions.cs
+++ src/Aco228.WebLLM/Aco228.AIGen.FalAi/ServiceExtensions.cs
@@ -13,6 +13,7 @@ public static class ServiceExtensions
     public static void RegisterFalAiServices(this IServiceCollection services)
         => typeof(ServiceExtensions).RegisterIfNot(() =>
         {
+            services.RegisterServicesFromAssembly(typeof(ServiceExtensions).Assembly);
             services.RegisterApiServices(typeof(ServiceExtensions).Assembly);
             
             services.RegisterPostBuildAction((pr) =>
